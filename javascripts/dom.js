3efecabfe3dbf320d8e18e7a19efff0076a75e29
Juno Lee <lejuno80@gmail.com>
2018-04-13 03:30:04 -0500

    add click event

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,15 +1,15 @@
-const domEvents = require('./events');
+const addEvents = require('./events');
 
 const outputDiv = document.getElementById('pet-container');
 
 const domString = (pets) => {
   let strang = '';
   pets.forEach((pet) => {
-    strang += `<div class="petCard">`;
+    strang += `<div class="petCard data-pet-id="${pet.id}">`;
     strang += `<p class="pet">${pet.name}</p>`;
     strang += `<img class="petImage" src="${pet.imageUrl}">`;
     strang += `<p class="petColor">${pet.color}</p>`;
-    strang += `<p class="petSkill">${pet.specialSkill}</p>`;
+    strang += `<p class="petType">${pet.type}</p>`;
     strang += `</div>`;
   });
   return strang;
@@ -17,7 +17,7 @@ const domString = (pets) => {
 
 const printToDom = (petArray) => {
   outputDiv.innerHTML = domString(petArray);
-  domEvents();
+  addEvents();
 };
 
 module.exports = printToDom;
